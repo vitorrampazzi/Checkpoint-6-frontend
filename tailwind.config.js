65b5b44fb2154c7252e18e4828346745c4ad05f0
vitorrampazzi <vitor.rampazzi@gmail.com>
2025-10-30 17:19:51 -0300

    Projeto Checkpoint 6 Front-End

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'custom-bg': '#F4F5F9',
+      },
+      fontFamily: {
+        'heebo': ['Heebo', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
